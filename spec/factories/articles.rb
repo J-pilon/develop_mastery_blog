55796a84d1a602f483534d3062fe09b5fb0575f5
Josiah <josiah.pilon@gmail.com>
2022-02-08 13:17:44 -0800

    Updates factory for article

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :article do
-    title {"This is a title."}
-    body {"This is the body of the article."}
+    title {"Test Title"}
+    body {"This is the test body of the article."}
   end
 end
