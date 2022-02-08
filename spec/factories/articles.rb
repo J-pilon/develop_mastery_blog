bd6eeb480a5ea391e6d7fe78078d9079b1a84e0f
Josiah <josiah.pilon@gmail.com>
2022-02-08 00:15:52 -0800

    Adds factories using FactoryBot

---
+++ spec/factories/articles.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :article do
+    title {"This is a title."}
+    body {"This is the body of the article."}
+  end
+end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,22 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+
   it "is valid with a title and body" do
-    article = Article.new(
-      title: "title",
-      body: "body"
-    )
-    expect(article).to be_valid
+    expect(FactoryBot.build(:article)).to be_valid
   end
 
   it "is invalid without a title" do 
-    article = Article.new(title: nil)
+    article = FactoryBot.build(:article, title: nil)
     article.valid?
     expect(article.errors[:title]).to include("can't be blank")
   end
 
   it "is invalid without a body" do
-    article = Article.new(body: nil)
+    article = FactoryBot.build(:article, body: nil)
     article.valid?
     expect(article.errors[:body]).to include("can't be blank")
   end
