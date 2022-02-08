66df27461db4d361164d6302144d4ccc5a546a72
Josiah <josiah.pilon@gmail.com>
2022-02-07 19:30:35 -0800

    Tests for Article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a title and body" do
+    article = Article.new(
+      title: "title",
+      body: "body"
+    )
+    expect(article).to be_valid
+  end
+
+  it "is invalid without a title" do 
+    article = Article.new(title: nil)
+    article.valid?
+    expect(article.errors[:title]).to include("can't be blank")
+  end
+
+  it "is invalid without a body" do
+    article = Article.new(body: nil)
+    article.valid?
+    expect(article.errors[:body]).to include("can't be blank")
+  end
 end
