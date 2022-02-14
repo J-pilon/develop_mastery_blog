3c2bd9aee5c2bd600e3ae69ce787247a116045cb
Josiah <josiah.pilon@gmail.com>
2022-02-14 15:26:07 -0800

    Updates seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do |i|
+100.times do |i|
     Article.create(
         title: "Article #{i + 1}",
-        body: 'this is the body'
+        body: LoremIpsum.w(50),
     )
 end
