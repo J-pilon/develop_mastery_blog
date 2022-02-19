38234682dbdfb2c03fbeecf4baa4d9b7b8be5451
Josiah P <77027615+J-pilon@users.noreply.github.com>
2022-02-19 10:08:16 -0800

    Feature-test/pagination (#23)
    * Tests pagination feature
    
    Co-authored-by: Josiah <josiah.pilon@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,6 @@ if ENV['CI'] == 'true'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
-require 'capybara/rspec'
-
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
