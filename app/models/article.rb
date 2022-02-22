59c6096a0d4ce96ee159a46747addef7b8f8281c
Josiah <josiah.pilon@gmail.com>
2022-02-22 15:11:38 -0800

    Creates slug from title. Overrides .to_param and .all to incorporate slug

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,21 @@
 class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
+    
+    after_validation :set_slug, only: [:create, :update]
+
+    def self.sluggable
+        all.extending(Sluggable::Finder)
+    end
+
+    def to_param
+        return nil unless persisted?
+        slug
+    end
+    
+    private
+
+    def set_slug
+        self.slug = title.to_s.parameterize
+    end
 end
