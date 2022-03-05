2a553dbaaac27dc94e63f65154cf7a99a768a5bf
Josiah <josiah.pilon@gmail.com>
2022-03-04 16:45:20 -0800

    Adds error message for user to see

--- app/javascript/components/articles/EditPage.jsx
+++ app/javascript/components/articles/EditPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import {Label, Input} from "reactstrap";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import ErrorBlock from "../ErrorBlock";
 
 
 const EditPage = (props) => {
 
 	return (
 		<div className="d-flex flex-column justify-content-center align-items-center">
+			<ErrorBlock
+				message={props.error}
+			/>
+
 			<form 
 				className="w-50 h-50 d-flex flex-column align-items-center p-3 border"
 				action={`/articles/${props.article.id}`} 
@@ -60,7 +65,8 @@ EditPage.propTypes = {
 	  title: PropTypes.string,
 	  body: PropTypes.string,
 	  id: PropTypes.number
-	}).isRequired
+	}).isRequired,
+	error: PropTypes.string,
   }
 
 export default EditPage;
