0749babb5a61f706dff8e397027d76a7d6d87dee
Josiah <josiah.pilon@gmail.com>
2022-03-23 16:23:43 -0700

    Adds tinymce rich text editor component

--- app/javascript/components/articles/NewPage.jsx
+++ app/javascript/components/articles/NewPage.jsx
@@ -1,6 +1,8 @@
-import React, {useState} from "react";
+import React, {useRef} from "react";
 import {Label, Input} from "reactstrap";
 import PropTypes from "prop-types"
+
+import { Editor } from '@tinymce/tinymce-react';
 
 
 const NewPage = (props) => {
@@ -15,24 +17,49 @@ const NewPage = (props) => {
 
 				<input type="hidden" name="authenticity_token" value={document.querySelector("[name=csrf-token]").content}/>
 
-				<Label>
-					Enter the articles title:
+				<input type="hidden" name="article[user_id]" value="1"/>
 
-					<Input
-						placeholder="title"
-						name="article[title]"
-						id="article_title"
-					/>
+
+				<Label>
+					Enter the articles title: 
+
+						<Input
+							placeholder="title"
+							name="article[title]"
+							id="article_title"
+						/>
+
+					
 				</Label>
 
 				<Label>
 					Enter the articles body:
 
-					<Input
+					<Editor
+						apiKey="82i1zra2emdhclzhbkp2fdtnf0oafyx5yxr89bkls8cqaaur"
+						textareaName="article[body]"
+						initialValue="<p>This is the initial content of the editor.</p>"
+						init={{
+						height: 500,
+						menubar: false,
+						plugins: [
+							'advlist autolink lists link image charmap print preview anchor',
+							'searchreplace visualblocks code fullscreen',
+							'insertdatetime media table paste code help wordcount'
+						],
+						toolbar: 'undo redo | formatselect | ' +
+						'bold italic backcolor | alignleft aligncenter ' +
+						'alignright alignjustify | bullist numlist outdent indent | ' +
+						'removeformat | help',
+						content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+						}}
+					/>
+
+					{/* <Input
 						placeholder="body"
 						name="article[body]"
 						id="article_body"
-					/>
+					/> */}
 				</Label>
 
 				<div>
